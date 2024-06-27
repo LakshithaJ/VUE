2569fc9386e6cb34522e99c5eef458c6ca90774e
LakshithaRJ <lakshrj139@gmail.com>
2024-06-27 13:39:37 -0500

    assignment

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: "Laksh",
+      age: 25,
+      imageURL:
+        "https://gratisography.com/wp-content/uploads/2024/03/gratisography-funflower-800x525.jpg",
+    };
+  },
+  methods: {},
+});
+
+app.mount("#assignment");
